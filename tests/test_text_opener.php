5922998a6ae1beb378e7da12cff8e0ef08d78238
Samuel Martin <Samuel Martin>
2019-09-18 22:54:08 +0100

    Test data setUp and tornDown in

--- tests/test_text_opener.php
+++ tests/test_text_opener.php
@@ -5,8 +5,16 @@ require '/home/sam/Programs/php/plagcheck/text_opener.php';
 class TextOpenerTest extends \PHPUnit_Framework_TestCase
 {
     protected $allTexts;
+
     protected function setUp(): void {
-        $this->allTexts = new Texts('~/Programs/php/plagcheck/dummy_input/text1.txt');
+        $fp = fopen('test_data.txt', 'w');
+        fwrite($fp, 'this is the first line\n');
+        fwrite($fp, '');
+        fwrite($fp, 'this is the last line');
+        fclose($fp);
+
+        $this->allTexts = new Texts('test_data.txt');
+        var_dump($this->allTexts);
     }
     public function testTextIsLoaded(){
         $text = $this->allTexts->text;
@@ -17,6 +25,9 @@ class TextOpenerTest extends \PHPUnit_Framework_TestCase
         $paras = $this->allTexts->paras;
         $this->assertNotEmpty($paras, $message = 'Text not loaded into paragraph array');
         $this->assertNotCount(0, [$paras]);
+    }
+    protected function tearDown(){
+        unlink('test_data.txt');
     }
 }
 
